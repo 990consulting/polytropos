890f8be0c3b744195f7aec27251aa79699b53147
quimeyv <quimeyv@gmail.com>
2019-04-22 14:20:08 -0300

    Implemented translation of primitives, test1 is passing

--- etl4/ontology/track/__track.py
+++ etl4/ontology/track/__track.py
@@ -13,7 +13,7 @@ class Track:
         """Convert specs into a Variable hierarchy, then construct a Track instance."""
         return Track(
             {
-                source_name: build_variable(variable_data)
-                for source_name, variable_data in specs.items()
+                variable_id: build_variable(variable_data)
+                for variable_id, variable_data in specs.items()
             }
         )
--- etl4/translate/__translate.py
+++ etl4/translate/__translate.py
@@ -1,9 +1,29 @@
+from addict import Dict as Addict
 from collections.abc import Callable
 from etl4.ontology.track import Track
 
 class Translate(Callable):
     def __init__(self, source: Track, target: Track):
-        pass
+        self.source = source
+        self.target = target
 
-    def __call__(self, *args, **kwargs):
-        pass
+    def __call__(self, document):
+        if isinstance(document, Addict):
+            # Addict has a weird behavior when a key is not present and returns
+            # a dict instead of None or throwing an exception
+            document = document.to_dict()
+        output_document = {}
+        # Translate all variables
+        for variable_id, variable in self.target.variables.items():
+            # Start with None as a default value
+            output_document[variable.name] = None
+            # Then try sources one by one
+            for source in variable.sources:
+                if source in self.source.variables:
+                    output_document[variable.name] = document.get(
+                        self.source.variables[source].name
+                    )
+                    # Terminate the search if an actual value is found
+                    if output_document[variable.name] is not None:
+                        break
+        return output_document
