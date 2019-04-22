7cedaee90406984de6379f55cb9e283105818440
quimeyv <quimeyv@gmail.com>
2019-04-22 13:02:04 -0300

    Change function signature

--- etl4/ontology/variable/__init__.py
+++ etl4/ontology/variable/__init__.py
@@ -3,6 +3,7 @@ from etl4.ontology.variable.__variable import Variable
 from dacite import from_dict
 
 
-def build_variable(data_type, data):
+def build_variable(data):
+    data_type = data['data_type']
     cls = getattr(etl4.ontology.variable.__variable, data_type)
     return from_dict(cls, data)
