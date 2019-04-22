625cc3bd08d5b1cf9f068f3bff732ef92c5e8a80
quimeyv <quimeyv@gmail.com>
2019-04-22 12:52:15 -0300

    Implemented variables with dispatch using dataclasses

--- etl4/ontology/variable/__init__.py
+++ etl4/ontology/variable/__init__.py
@@ -1,1 +1,8 @@
+import etl4.ontology.variable.__variable
 from etl4.ontology.variable.__variable import Variable
+from dacite import from_dict
+
+
+def build(data_type, data):
+    cls = getattr(etl4.ontology.variable.__variable, data_type)
+    return from_dict(cls, data)
--- etl4/ontology/variable/__variable.py
+++ etl4/ontology/variable/__variable.py
@@ -1,2 +1,76 @@
+from dataclasses import dataclass, make_dataclass
+from typing import List, Dict
+from etl4.ontology.schemas import DATA_TYPES
+
+
+@dataclass
 class Variable:
+    name: str
+    sort_order: int
+    sources: List[str]
+    parent: str
+    source_child_mappings: Dict[str, Dict[str, List[str]]]
+
+
+@dataclass
+class Container(Variable):
     pass
+
+
+@dataclass
+class Integer(Variable):
+    pass
+
+
+@dataclass
+class Text(Variable):
+    pass
+
+
+@dataclass
+class Decimal(Variable):
+    pass
+
+
+@dataclass
+class Unary(Variable):
+    pass
+
+
+@dataclass
+class Binary(Variable):
+    pass
+
+
+@dataclass
+class Currency(Variable):
+    pass
+
+
+@dataclass
+class Phone(Variable):
+    pass
+
+
+@dataclass
+class Email(Variable):
+    pass
+
+
+@dataclass
+class URL(Variable):
+    pass
+
+
+class Folder(Container):
+    pass
+
+
+class List(Container):
+    pass
+
+
+class NamedList(Container):
+    pass
+
+
