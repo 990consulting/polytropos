bf8645cbadd1095377488a129e12053eac6c03cb
quimeyv <quimeyv@gmail.com>
2019-04-22 12:59:06 -0300

    Change function name

--- etl4/ontology/variable/__init__.py
+++ etl4/ontology/variable/__init__.py
@@ -3,6 +3,6 @@ from etl4.ontology.variable.__variable import Variable
 from dacite import from_dict
 
 
-def build(data_type, data):
+def build_variable(data_type, data):
     cls = getattr(etl4.ontology.variable.__variable, data_type)
     return from_dict(cls, data)
