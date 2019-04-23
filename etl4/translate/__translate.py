2f3e250b24127bb0d15028fbdffc65f398eb3773
quimeyv <quimeyv@gmail.com>
2019-04-23 13:02:36 -0300

    Implemented list and named list translation

--- etl4/translate/__translate.py
+++ etl4/translate/__translate.py
@@ -15,11 +15,11 @@ class Translate(Callable):
                 variable.parent
             ][variable_id] = variable
 
-    def find_in_document(self, variable_id, document):
+    def find_in_document(self, variable_id, document, parent=''):
         if document is None:
             return None
         variable = self.source.variables[variable_id]
-        if variable.parent:
+        if variable.parent != parent:
             parent = self.find_in_document(
                 variable.parent,
                 document
@@ -29,44 +29,80 @@ class Translate(Callable):
             return parent.get(variable.name)
         return document.get(variable.name)
 
-    def translate_generic(self, variable_id, variable, document):
-        # Try sources one by one
+    def replace_sources(self, source_data):
+        for variable_id, sources in source_data.items():
+            self.target.variables[variable_id].sources = sources
+
+    def translate_generic(self, variable_id, variable, document, parent):
         for source in sorted(
             variable.sources,
             key=lambda source: self.source.variables[source].sort_order
         ):
-            result = self.find_in_document(source, document)
+            result = self.find_in_document(source, document, parent)
             if result is not None:
                 return result
         return None
 
-    def translate_folder(self, variable_id, variable, document):
-        return self(document, variable_id)
+    def translate_folder(self, variable_id, variable, document, parent):
+        return self(document, variable_id, parent)
 
-    def translate_list(self, variable_id, variable, document):
-        pass
+    def translate_list(self, variable_id, variable, document, parent):
+        results = []
+        for source in sorted(
+            variable.sources,
+            key=lambda source: self.source.variables[source].sort_order
+        ):
+            list_source = self.find_in_document(source, document, parent)
+            self.replace_sources(variable.source_child_mappings[source])
+            if list_source is None:
+                continue
+            for value in list_source:
+                results.append(
+                    self(
+                        {self.source.variables[source].name: value},
+                        variable_id,
+                        self.source.variables[source].name
+                    )
+                )
+        return results
 
-    def translate_named_list(self, variable_id, variable, document):
-        pass
+    def translate_named_list(self, variable_id, variable, document, parent):
+        results = {}
+        for source in sorted(
+            variable.sources,
+            key=lambda source: self.source.variables[source].sort_order
+        ):
+            list_source = self.find_in_document(source, document, parent)
+            self.replace_sources(variable.source_child_mappings[source])
+            if list_source is None:
+                continue
+            for key, value in list_source.items():
+                results[key] = self(
+                    {self.source.variables[source].name: value},
+                    variable_id,
+                    self.source.variables[source].name
+                )
+        return results
 
-    def __call__(self, document, parent=''):
+    def get_translate_function(self, variable):
+        if isinstance(variable, NamedList):
+            return self.translate_named_list
+        elif isinstance(variable, List):
+            return self.translate_list
+        elif isinstance(variable, Folder):
+            return self.translate_folder
+        else:
+            return self.translate_generic
+
+    def __call__(self, document, parent='', source_parent=''):
         output_document = {}
         # Translate all variables with the same parent
         for variable_id, variable in self.target_variables_by_parent[
             parent
         ].items():
-            translate = None
-            if isinstance(variable, NamedList):
-                translate = self.translate_named_list
-            elif isinstance(variable, List):
-                translate = self.translate_list
-            elif isinstance(variable, Folder):
-                translate = self.translate_folder
-            else:
-                translate = self.translate_generic
-
+            translate = self.get_translate_function(variable)
             output_document[variable.name] = translate(
-                variable_id, variable, document
+                variable_id, variable, document, source_parent
             )
 
         return output_document
