0289d5655a5ffb67512765b9eb39fce2e9d4f77b
quimeyv <quimeyv@gmail.com>
2019-04-23 22:50:17 -0300

    Add test for find_in_document

---
+++ test/test_integration/translate/test_find_in_document.py
@@ -0,0 +1,129 @@
+import pytest
+from typing import Dict, Tuple, List
+from etl4.ontology.track import Track
+from etl4.translate import Translate
+import itertools
+
+
+@pytest.fixture()
+def source_doc() -> Dict:
+    return {
+        "my_folder": {
+            "day": "Tuesday",
+            "the_folder": {
+                "name": "Steve",
+                "color": "orange"
+            }
+        },
+        "meaning_of_life": 42,
+        "root": {"nested": {"deeper": "nothing"}}
+    }
+
+
+@pytest.fixture()
+def source_spec() -> Dict:
+    return {
+        "source_folder": {
+            "name": "my_folder",
+            "data_type": "Folder",
+            "sort_order": 0
+        },
+        "source_folder_folder": {
+            "name": "the_folder",
+            "data_type": "Folder",
+            "parent": "source_folder",
+            "sort_order": 1
+        },
+        "source_folder_name": {
+            "name": "name",
+            "data_type": "Text",
+            "parent": "source_folder_folder",
+            "sort_order": 0
+        },
+        "source_folder_color": {
+            "name": "color",
+            "data_type": "Text",
+            "parent": "source_folder_folder",
+            "sort_order": 1
+        },
+        "source_meaning_of_life": {
+            "name": "meaning_of_life",
+            "data_type": "Integer",
+            "sort_order": 1
+        },
+        "source_root": {
+            "name": "root",
+            "data_type": "Folder",
+            "sort_order": 0
+        },
+        "source_nested": {
+            "name": "nested",
+            "data_type": "Folder",
+            "parent": "source_root",
+            "sort_order": 0
+        },
+        "source_deeper": {
+            "name": "deeper",
+            "data_type": "Text",
+            "parent": "source_nested",
+            "sort_order": 0
+        }
+    }
+
+
+@pytest.fixture()
+def target_spec() -> Dict:
+    return {}
+
+
+@pytest.fixture()
+def translate(source_spec: Dict, target_spec: Dict) -> Translate:
+    source_track: Track = Track.build(source_spec)
+    target_track: Track = Track.build(target_spec)
+    translate: Translate = Translate(source_track, target_track)
+    return translate
+
+
+def test_base(source_doc, translate):
+    actual = translate.find_in_document('source_meaning_of_life', source_doc)
+    expected = 42
+    assert actual == expected
+
+
+def test_deep(source_doc, translate):
+    actual = translate.find_in_document('source_deeper', source_doc)
+    expected = 'nothing'
+    assert actual == expected
+    actual = translate.find_in_document('source_folder_color', source_doc)
+    expected = 'orange'
+    assert actual == expected
+
+
+def test_parent(source_doc, translate):
+    actual = translate.find_in_document(
+        'source_folder_color', source_doc['my_folder'], 'source_folder'
+    )
+    expected = 'orange'
+    assert actual == expected
+
+
+def test_parent_no_parent(source_doc, translate):
+    actual = translate.find_in_document(
+        'source_folder_color', source_doc['my_folder']
+    )
+    assert actual is None
+
+
+def test_parent_immediate(source_doc, translate):
+    actual = translate.find_in_document(
+        'source_folder_color', source_doc['my_folder']['the_folder'], 'source_folder_folder'
+    )
+    expected = 'orange'
+    assert actual == expected
+
+
+def test_parent_immediate_no_parent(source_doc, translate):
+    actual = translate.find_in_document(
+        'source_folder_color', source_doc['my_folder']['the_folder']
+    )
+    assert actual is None
