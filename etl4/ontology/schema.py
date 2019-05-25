d75a12b7fbcc1b55dfa7c39344bc9af9e8a1491f
quimeyv <quimeyv@gmail.com>
2019-05-25 15:19:31 -0300

    Load data on task build

--- etl4/ontology/schema.py
+++ etl4/ontology/schema.py
@@ -1,8 +1,27 @@
+import os
+import json
 from etl4.ontology.track import Track
 from dataclasses import dataclass
+
+SCHEMAS_DIR = 'fixtures/conf/schemas/'
 
 @dataclass
 class Schema:
     """A schema identifies all of the temporal and invariant properties that a particular entity can have."""
     temporal: Track
     invariant: Track
+
+    @classmethod
+    def load(cls, path):
+        if path is None:
+            return None
+        with open(
+                os.path.join(SCHEMAS_DIR, path, 'temporal.json'), 'r'
+        ) as temporal:
+            with open(
+                    os.path.join(SCHEMAS_DIR, path, 'invariant.json'), 'r'
+            ) as invariant:
+                return cls(
+                    temporal=json.load(temporal),
+                    invariant=json.load(invariant)
+                )
