3827c75e5591b28f6618cb552108737bb3e96426
quimeyv <quimeyv@gmail.com>
2019-05-29 14:57:00 +0200

    Fix test to use all scenarios and several typos

--- test/test_integration/test_task.py
+++ test/test_integration/test_task.py
@@ -1,10 +1,19 @@
+import pytest
 import json
 import os
 from etl4.ontology.task import Task
 
 
-def test_infer_about_person():
-    task = Task.build('1_mm_only', 'infer_about_person')
+@pytest.mark.parametrize(
+    'scenario,task_name',
+    [
+        ('1_mm_only', 'infer_about_person'),
+        ('2_mm_scan', 'bmi_rank'),
+        ('3_mm_aggregate_mm_scan', 'economy'),
+    ]
+)
+def test_task(scenario, task_name):
+    task = Task.build(scenario, task_name)
     task.run()
     path = os.path.join(task.path_locator.entities_dir, 'person')
     for filename in os.listdir(os.path.join(path, 'actual')):
