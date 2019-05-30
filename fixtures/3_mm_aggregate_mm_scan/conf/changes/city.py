fbdd018de529acba2b6f95b662e93a41ee34cb2a
quimeyv <quimeyv@gmail.com>
2019-05-30 12:39:03 +0200

    Reorganize folders

--- fixtures/3_mm_aggregate_mm_scan/conf/changes/city.py
+++ fixtures/3_mm_aggregate_mm_scan/conf/changes/city.py
@@ -16,6 +16,6 @@ class CalculateMeanProductivity(Change):
 
     def __call__(self, composite: Dict):
         periods: List[str] = list(composites.get_periods(composite))
-        annual_prods = (composites.get_observation(composite, period, self.annual_prod_var) for period in periods)
+        annual_prods = [composites.get_observation(composite, period, self.annual_prod_var) for period in periods]
         mean_prod = numpy.average(annual_prods)
         composites.put_property(composite, self.mean_prod_var, mean_prod)
