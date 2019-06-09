8e4c45bbb81391b3b45a57205dbea90a401e836d
quimeyv <quimeyv@gmail.com>
2019-06-09 22:58:06 +0200

    Preparing for pypi - improve setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,16 @@ with open('requirements.txt') as f:
 setuptools.setup(
     name="polytropos",
     version="0.0.1",
+    license='agpl-3.0',
+    url='https://github.com/borenstein/polytropos',
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
-    install_requires=required
+    install_requires=required,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.7',
+    ]
 )
