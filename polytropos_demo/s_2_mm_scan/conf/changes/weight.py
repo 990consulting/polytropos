4d0b9ff2b3f2aeafd6fd51ceb3eb6151b4193b2a
David Bruce Borenstein <david@appliednonprofitresearch.com>
2019-06-18 21:38:24 -0400

    Migrated scenario 2 back in, and it is not yet working (as expected). 320 of 321 tests pass.

---
+++ polytropos_demo/s_2_mm_scan/conf/changes/weight.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+from typing import Dict
+
+import scipy.stats
+import numpy as np
+
+from polytropos.actions.evolve import Change
+from polytropos.actions.validator import VariableValidator
+from polytropos.ontology.variable import Decimal
+from polytropos.util import composites
+
+
+@dataclass
+class AssignRegressionStats(Change):
+    annual_weight_var: Decimal = VariableValidator(data_type=Decimal, temporal=1)
+    weight_slope_var: Decimal = VariableValidator(data_type=Decimal, temporal=-1)
+    weight_pval_var: Decimal = VariableValidator(data_type=Decimal, temporal=-1)
+
+    def __call__(self, composite: Dict):
+        years = sorted([int(year) for year in composites.get_periods(composite)])
+        weights = [composites.get_observation(composite, str(year), self.annual_weight_var) for year in years]
+        slope, intercept, r_value, p_value, std_err = scipy.stats.linregress(
+            np.asarray(years), np.asarray(weights)
+        )
+        composites.put_property(composite, self.weight_slope_var, slope)
+        composites.put_property(composite, self.weight_pval_var, p_value)
--- test/test_scenarios/test_scenario_1.py
+++ test/test_scenarios/test_scenario_1.py
@@ -1,5 +1,6 @@
 import logging
 
+# noinspection PyUnresolvedReferences
 def test(run_task):
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
     import polytropos_demo.s_1_mm_only.conf.changes.vitals
