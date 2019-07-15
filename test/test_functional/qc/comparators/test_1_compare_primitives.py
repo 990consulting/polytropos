9dbff505936260196c6ef83f78b2f0485a7aeefb
David Bruce Borenstein <david@appliednonprofitresearch.com>
2019-07-15 18:21:48 -0400

    Implemented business logic and some tests for comparators. 439 of 459 tests pass.

---
+++ test/test_functional/qc/comparators/test_1_compare_primitives.py
@@ -0,0 +1,24 @@
+import pytest
+from typing import Optional, Any
+from polytropos.tools.qc.comparators import compare_primitives
+
+@pytest.mark.parametrize("fixture, actual, expected", [
+    (None, None, True),
+    (0, 0.0, True),
+    (-1.0, -1, True),
+    (0.001, 0.001000, True),
+    ("A", "A", True),
+    (True, True, True),
+    (False, False, True),
+    ("A", "a", False),
+    (None, 0, False),
+    (None, 0.0, False),
+    (None, 0.1, False),
+    (None, True, False),
+    (None, False, False),
+    ("a", None, False),
+    (0, None, False)
+])
+def test_compare_primitives(fixture: Optional[Any], actual: Optional[Any], expected: bool):
+    observed: bool = compare_primitives(fixture, actual)
+    assert observed == expected
---
+++ test/test_functional/qc/comparators/test_3_compare_lists.py
@@ -0,0 +1,61 @@
+import pytest
+
+def test_empty_lists():
+    pytest.fail()
+
+def test_identical_lists():
+    pytest.fail()
+
+def test_actual_has_more():
+    pytest.fail()
+
+def test_fixture_has_more():
+    pytest.fail()
+
+def test_mismatch_in_one_element():
+    pytest.fail()
+
+def test_extra_key_in_actual_element():
+    pytest.fail()
+
+def test_extra_key_in_fixture_element():
+    pytest.fail()
+
+def test_nested_folder_identical():
+    pytest.fail()
+
+def test_nested_folder_only_in_fixture():
+    pytest.fail()
+
+def test_nested_folder_only_in_actual():
+    pytest.fail()
+
+def test_nested_folder_contains_mismatch():
+    pytest.fail()
+
+def test_nested_list_identical():
+    pytest.fail()
+
+def test_nested_list_length_mismatch():
+    pytest.fail()
+
+def test_nested_list_extra_value_in_actual():
+    pytest.fail()
+
+def test_nested_list_extra_value_in_fixture():
+    pytest.fail()
+
+def test_nested_named_list_identical():
+    pytest.fail()
+
+def test_nested_named_list_key_mismatch():
+    pytest.fail()
+
+def test_nested_named_list_extra_value_in_actual_element():
+    pytest.fail()
+
+def test_nested_named_list_extra_value_in_fixture():
+    pytest.fail()
+
+def test_nested_named_list_only_in_fixture():
+    pytest.fail()
