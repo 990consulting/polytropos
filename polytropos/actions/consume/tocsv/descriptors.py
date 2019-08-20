de1282fecac824a1d7403ea38bffe4a41982e3c4
David Bruce Borenstein <david@appliednonprofitresearch.com>
2019-08-20 08:29:15 -0400

    Added freelancer's work on test_2_yaml_to_blocks.

--- polytropos/actions/consume/tocsv/descriptors.py
+++ polytropos/actions/consume/tocsv/descriptors.py
@@ -1,12 +1,47 @@
 from dataclasses import dataclass
-from typing import List, Tuple
+from typing import List, Tuple, Optional, Iterator, Union
 
 from polytropos.ontology.schema import Schema
 
 class DescriptorsToBlocks:
     """Convert a list of column descriptors to column blocks, which can in turn be used to extract data."""
     def __call__(self, descriptors: List) -> Tuple:
-        pass
+        return tuple(self._convert_list(descriptors))
+
+    def _convert_list(self, descriptors: List, list_name: Optional[str] = None) -> Iterator[Union[str, Tuple]]:
+        """
+        :param descriptors: A list of things to convert to a block
+        :param list_name:   A name for the current sublist - if supplied, it's prepended to the converted list.
+                            (It's done in the function to skip chaining the iterators when calling the function.)
+        """
+        if list_name is not None:
+            yield list_name
+
+        for item in descriptors: # type: Union[str, dict]
+            if isinstance(item, str):
+                yield item
+            elif isinstance(item, dict):
+                # TODO what to do with multiple items in a dict?
+                for inner_key, inner_item in item.items(): # type: str, Union[str, dict]
+                    if isinstance(inner_item, str):
+                        # If a list item is a dictionary with a single key-value pair, then the key is the actual data
+                        # to be put in the column and the value is the name of the column
+                        # TODO check that the item is the only thing in the dict
+                        yield inner_key
+                    elif isinstance(inner_item, dict):
+                        if 'type' in inner_item and inner_item['type'] in ('List', 'NamedList'):
+                            if 'children' in inner_item:
+                                yield tuple(self._convert_list(inner_item['children'], inner_key))
+                            else:
+                                yield (inner_key,)
+                        else:
+                            raise Exception('Unrecognized structure of the descriptor: a dict must be marked with '
+                                            'type List or NamedList')
+                    else:
+                        raise Exception("Unrecognized structure of the descriptor: can't convert an item of type " + type(inner_item))
+            else:
+                # TODO should test the handling of unknown types
+                raise Exception("Unrecognized structure of the descriptor: can't convert an item of type " + type(item))
 
 @dataclass
 class DescriptorsToColumnNames:
