e03918f7aa0d181cda014765d78737c57f314931
David Bruce Borenstein <david@appliednonprofitresearch.com>
2019-09-27 19:30:03 -0400

    Implemented DoesNotExist filter.
    I plan to migrate Exist/DoesNotExist to UnivariateFilter, and then
    adding more functionality to the base class.

---
+++ test/test_functional/action/filter/existence/does_not_exist/test_does_not_exist_both.py
@@ -0,0 +1,26 @@
+import copy
+from typing import Callable, List
+
+import pytest
+
+from polytropos.actions.filter import Filter
+from polytropos.actions.filter.mem import InMemoryFilterIterator
+from polytropos.ontology.composite import Composite
+
+from polytropos.actions.filter.exists import DoesNotExist
+from polytropos.ontology.variable import VariableId
+
+@pytest.fixture()
+def do_test(schema, composites, context) -> Callable:
+    def _do_test(var_id: VariableId, expected: List[Composite]):
+        the_filter: Filter = DoesNotExist(context, schema, var_id)
+        f_iter: Callable = InMemoryFilterIterator([the_filter])
+        actual: List[Composite] = list(f_iter(composites))
+        assert actual == expected
+    return _do_test
+
+@pytest.mark.parametrize("var_id", ["t_Text", "t_Folder", "t_List", "t_KeyedList"])
+def test_all(var_id, composites, do_test):
+    expected: List[Composite] = [copy.copy(c) for c in composites]
+    del expected[0].content["period_1"]
+    do_test(var_id, expected)
--- test/test_functional/action/filter/existence/does_not_exist/test_does_not_exist_only_narrow.py
+++ test/test_functional/action/filter/existence/does_not_exist/test_does_not_exist_only_narrow.py
@@ -1,3 +1,4 @@
+import copy
 from typing import Callable, List
 
 import pytest
@@ -6,20 +7,20 @@ from polytropos.actions.filter import Filter
 from polytropos.actions.filter.mem import InMemoryFilterIterator
 from polytropos.ontology.composite import Composite
 
-from polytropos.actions.filter.exists import Exists
+from polytropos.actions.filter.exists import DoesNotExist
 from polytropos.ontology.variable import VariableId
 
 @pytest.fixture()
 def do_test(schema, composites, context) -> Callable:
     def _do_test(var_id: VariableId, expected: List[Composite]):
-        the_filter: Filter = Exists(context, schema, var_id, narrows=False)
+        the_filter: Filter = DoesNotExist(context, schema, var_id, filters=False)
         f_iter: Callable = InMemoryFilterIterator([the_filter])
         actual: List[Composite] = list(f_iter(composites))
         assert actual == expected
     return _do_test
 
 @pytest.mark.parametrize("var_id", ["t_Text", "t_Folder", "t_List", "t_KeyedList"])
-def test_all(var_id: str, composites, do_test):
-    var_id: str = "t_Text"
-    expected = [composites[0]]
+def test_all(var_id, composites, do_test):
+    expected: List[Composite] = [copy.copy(c) for c in composites]
+    del expected[0].content["period_1"]
     do_test(var_id, expected)
