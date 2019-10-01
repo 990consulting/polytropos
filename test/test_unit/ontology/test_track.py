3de8dd2b0e2c018bcb31c22b3e2ec27ecdb2f1f5
Andrey Sokolov <falconandy@yandex.ru>
2019-10-01 12:08:26 +0400

    Variable "metadata"

---
+++ test/test_unit/ontology/test_track.py
@@ -0,0 +1,57 @@
+from typing import Dict
+
+import pytest
+
+from polytropos.ontology.track import Track
+from polytropos.ontology.variable import VariableId
+
+
+def test_correct_variable_spec():
+    spec: Dict = {
+        "var1": {
+            "name": "name1",
+            "data_type": "Text",
+            "sort_order": 1
+        },
+        "var2": {
+            "name": "name2",
+            "data_type": "Integer",
+            "sort_order": 0,
+            "metadata": {
+                "notes": "notes2"
+            }
+        }
+    }
+    track = Track.build(spec, None, "")
+
+    var1 = track[VariableId("var1")]
+    assert var1.name == "name1"
+    assert var1.data_type == "Text"
+    assert var1.sort_order == 1
+    assert var1.metadata == {}
+
+    var2 = track[VariableId("var2")]
+    assert var2.name == "name2"
+    assert var2.data_type == "Integer"
+    assert var2.sort_order == 0
+    assert var2.metadata == {
+        "notes": "notes2"
+    }
+
+
+def test_variable_spec_unexpected_fields():
+    spec: Dict = {
+        "var2": {
+            "name": "name2",
+            "data_type": "Integer",
+            "sort_order": 0,
+            "short_description": "descr3",
+            "comment": "comment2",
+            "metadata": {
+                "notes": "notes2"
+            }
+        }
+    }
+
+    with pytest.raises(ValueError, match=r"unexpected variable fields: \['comment', 'short_description'\]"):
+        Track.build(spec, None, "")
