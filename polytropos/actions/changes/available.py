f6c15120e72462697d28b314bd35b0b730b0a4bc
Andrey Sokolov <falconandy@yandex.ru>
2020-04-09 17:56:26 +0400

    BestAvailableV2

--- polytropos/actions/changes/available.py
+++ polytropos/actions/changes/available.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass, field
-from typing import Optional, Any, List
+from typing import Optional
 
+from polytropos.actions.changes.available_v2 import BestAvailableV2
 from polytropos.actions.evolve import Change
 from polytropos.ontology.composite import Composite
 from polytropos.ontology.variable import VariableId
-from polytropos.util.nesteddicts import MissingDataError
+
 
 @dataclass  # type: ignore
 class BestAvailable(Change):
@@ -16,25 +17,12 @@ class BestAvailable(Change):
     immutable_source: Optional[VariableId] = field(default=None)
     use_older_periods: bool = field(default=True)
 
-    def __call__(self, composite: Composite) -> None:
+    def __post_init__(self) -> None:
+        sources = []
         if self.immutable_source is not None:
-            try:
-                value: Optional[Any] = composite.get_immutable(self.immutable_source)
-                if value is not None:
-                    composite.put_immutable(self.target, value)
-                    return
-            except MissingDataError:
-                pass
+            sources.append(self.immutable_source)
+        sources.append(self.temporal_source)
+        self.bestAvailableV2 = BestAvailableV2(self.schema, self.lookups, self.target, sources, not self.use_older_periods)
 
-        to_consider: List[str] = sorted(composite.periods, reverse=True)
-        if not self.use_older_periods and len(to_consider) > 0:
-            to_consider = [to_consider[0]]
-
-        for period in to_consider:
-            try:
-                value = composite.get_observation(self.temporal_source, period)
-                if value is not None:
-                    composite.put_immutable(self.target, value)
-                    return
-            except MissingDataError:
-                continue
+    def __call__(self, composite: Composite) -> None:
+        self.bestAvailableV2(composite)
